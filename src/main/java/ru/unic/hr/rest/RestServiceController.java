2280d6c32b4ba2726e0adaa5eb1fef380e003229
supercep <supercep110@gmail.com>
2019-08-21 14:58:39 +0300

    Update rest service

--- src/main/java/ru/unic/hr/rest/RestServiceController.java
+++ src/main/java/ru/unic/hr/rest/RestServiceController.java
@@ -4,9 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ru.unic.hr.model.Item;
 import ru.unic.hr.model.Model;
 import ru.unic.hr.service.parser.VacancyParser;
 import ru.unic.hr.utils.*;
+
+import java.util.List;
 
 /**
  * Created by BritikovMI on 15.08.2019.
@@ -15,11 +18,11 @@ import ru.unic.hr.utils.*;
 public class RestServiceController {
 
     @GetMapping("/search/{params}")
-    public Model search(@PathVariable("params") String params)  {
+    public List<Item> search(@PathVariable("params") String params)  {
         VacancyParser parser = new VacancyParser();
         Model model = parser.parse(HttpRequest.doGet("https://api.hh.ru/vacancies?text=" + params).toString());
 
-        return model;
+        return model.getItems();
     }
 
     @RequestMapping("/hello")
